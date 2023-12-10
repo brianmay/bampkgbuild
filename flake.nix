5e5ad3d2ed58df1fd73f26dc7d2916eef25f2c6f
Brian May <brian@linuxpenguins.xyz>
2023-12-11 09:11:38 +1100

    Update flake inputs

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Build Debian packages in containers";
 
   inputs.flake-utils.url = "github:numtide/flake-utils";
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
   inputs.poetry2nix = {
     url = "github:nix-community/poetry2nix";
     inputs.nixpkgs.follows = "nixpkgs";
@@ -12,24 +12,26 @@
     flake-utils.lib.eachDefaultSystem (system:
       let
         # see https://github.com/nix-community/poetry2nix/tree/master#api for more functions and examples.
-        inherit (poetry2nix.legacyPackages.${system}) mkPoetryApplication;
+        p2n = import poetry2nix { inherit pkgs; };
+        mkPoetryApplication = p2n.mkPoetryApplication;
         pkgs = nixpkgs.legacyPackages.${system};
-        customOverrides = self: super: {
-          python-debian = super.python-debian.overrideAttrs
-            (old: { buildInputs = old.buildInputs ++ [ self.setuptools ]; });
-        };
       in {
         packages = {
           myapp = mkPoetryApplication {
             projectDir = self;
-            overrides =
-              [ pkgs.poetry2nix.defaultPoetryOverrides customOverrides ];
+            #overrides =
+            #  [ pkgs.poetry2nix.defaultPoetryOverrides customOverrides ];
+            overrides = p2n.overrides.withDefaults (final: prev: {
+              python-debian = prev.python-debian.overridePythonAttrs
+                (oldAttrs: {
+                  buildInputs = oldAttrs.buildInputs ++ [ final.setuptools ];
+                });
+            });
           };
           default = self.packages.${system}.myapp;
         };
 
-        devShells.default = pkgs.mkShell {
-          packages = [ poetry2nix.packages.${system}.poetry pkgs.podman ];
-        };
+        devShells.default =
+          pkgs.mkShell { packages = [ pkgs.poetry pkgs.podman ]; };
       });
 }
